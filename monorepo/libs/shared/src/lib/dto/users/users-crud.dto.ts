b1a0ae9fbaf727f378658b662eb9b6a83f50c6cf
ASUS\ASUS <lthkim01@gmail.com>
2023-03-07 21:10:43 +0700

    update user, pkg DTO

--- monorepo/libs/shared/src/lib/dto/users/users-crud.dto.ts
+++ monorepo/libs/shared/src/lib/dto/users/users-crud.dto.ts
@@ -1,10 +1,35 @@
+import { OmitType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsMobilePhone, IsEmail } from 'class-validator';
 import { BaseResDto } from '../base.dto';
 
 export class CreateUserReqDto {
   id: string;
+
+  @ApiProperty({
+    minLength: 3,
+    maxLength: 30,
+    nullable: false,
+    required: true
+  })
+  @IsString()
   name: string;
+
+  @ApiProperty()
+  @IsMobilePhone('vi-VN')
   phone: string;
+
+  @ApiProperty({
+    required: true
+  })
+  @IsEmail()
   email: string;
 }
 
 export class CreateUserResDto extends BaseResDto {}
+
+export class UpdateUserReqDto extends OmitType(CreateUserReqDto, ['email'] as const) {}
+
+export class UpdateUserResDto extends BaseResDto {}
+
+
