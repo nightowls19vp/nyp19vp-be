be1ffb64357dc25961b26732a32bb91d24dbe47b
MyrtilleKim <lthkim01@gmail.com>
2023-05-31 19:36:48 +0700

    emit to specific user

--- monorepo/apps/api-gateway/src/app/txn/txn.module.ts
+++ monorepo/apps/api-gateway/src/app/txn/txn.module.ts
@@ -9,6 +9,7 @@ import * as dotenv from 'dotenv';
 import { ENV_FILE } from '@nyp19vp-be/shared';
 import { SocketModule } from '../socket/socket.module';
 import { CommModule } from '../comm/comm.module';
+import { SocketGateway } from '../socket/socket.gateway';
 import { SocketService } from '../socket/socket.service';
 dotenv.config({
   path: process.env.NODE_ENV !== 'dev' ? process.env.ENV_FILE : ENV_FILE.DEV,
@@ -38,6 +39,7 @@ dotenv.config({
   providers: [
     TxnService,
     { provide: 'ZALOPAY_CONFIG', useValue: zpconfig },
+    SocketGateway,
     SocketService,
   ],
 })
