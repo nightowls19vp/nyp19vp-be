6abfe68045b05f85a55f93ca29cbe76cda675fd4
Ngô Minh Phát <nmphat01062001@gmail.com>
2023-08-17 01:16:05 +0700

    hotfix: nx 16.7.0, run-many got stuck

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd monorepo/docker/ && ./run.sh
 
-cd ../../ && cd monorepo && npm i --legacy-peer-deps && npx nx run-many --target=serve --all --maxParallel=100
+cd ../../ && cd monorepo && npm i --legacy-peer-deps -y && npm install --global nx@15.9.4 && nx run-many --target=serve --all --maxParallel=100
